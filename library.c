ea3330f1285c95986229ff75abb29f5ec7363e18
Onesmas-n <90699745+Onesmas-n@users.noreply.github.com>
2021-10-22 14:32:53 +0300

    Add files via upload
    library management project

---
+++ library.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("zetech library\n");
+    printf("Welcome onesmas\n");
+    printf("what would you like to do\n");
+    printf("1.Add a new user\n");
+    printf("2.Get a new book\n");
+    return 0;
+}
